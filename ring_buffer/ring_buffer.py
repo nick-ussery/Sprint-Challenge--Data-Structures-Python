9ca5b1b1352dcfa4494ed84b75c17573d9988104
nick-ussery <nick.ussery@gmail.com>
2020-08-14 11:06:05 -0500

    ring buffer and names done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.contents = []  # list of contents
+        self.age = []  # list of contents in the order added
 
     def append(self, item):
-        pass
+
+        if len(self.contents) < self.capacity:
+            # basic adding of elements before capacity reached
+            self.contents.append(item)
+            self.age.append(item)
+        else:  # if capacity is reached
+            old_item_location = self.contents.index(self.age[0])
+            self.age.pop(0)
+            self.age.append(item)
+            self.contents[old_item_location] = item
 
     def get(self):
-        pass
+        return self.contents
