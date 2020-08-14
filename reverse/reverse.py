cbeee08b75cda1e15eea77352412594153c007f5
nick-ussery <nick.ussery@gmail.com>
2020-08-14 11:57:44 -0500

    mvp on reverse list

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -39,4 +40,26 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        # new_list = LinkedList()
+        # current_node = node
+        # length = 0
+        # while current_node.get_next() != None:
+        #     print(f"Current Node: {current_node.get_value()}")
+        #     current_node = current_node.get_next()
+        #     length += 1
+        # new_list.add_to_head(current_node)
+        # for i in range(length):
+        #     while current_node.next_node != new_list.head or current_node != None:
+        #         current_node = current_node.get_next()
+        #     new_list.add_to_head(current_node)
+        # new_list.add_to_head(prev)
+        # return new_list
+        if node == None:
+            return
+
+        old_next = node.get_next()
+        node.set_next(prev)
+
+        if old_next != None:
+            self.head = old_next
+            self.reverse_list(old_next, node)
